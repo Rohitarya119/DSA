83edeeccf3ad1541c1548986dd7767ab409586df
Rohitarya119 <raviarya201016@gmail.com>
2024-09-17 17:01:08 +0530

    Basic java question

---
+++ BasicJavaQuestion.java
@@ -0,0 +1,171 @@
+import java.util.*;
+public class BasicJavaQuestion {
+    public static void main(String[] args) {
+         
+        Scanner sc= new Scanner(System.in);
+        // int n= sc.nextInt();
+// 
+
+// Positive negative
+        // if(n>0){
+        //     System.out.println("Number is positive");
+        // }
+        // else if(n==0){
+        //     System.out.println("Number is zero");
+        // }
+        // else{
+        //     System.out.println("Number is negative");
+        // }
+
+
+// //Even or odd 
+        // if(n%2==0){
+        //     System.out.println("number is even");
+        // }
+        // else{
+        //     System.out.println("number is odd");
+        // }
+        
+
+
+// vowel or constant
+        // String b= sc.nextLine();
+        // char s=b.charAt(0);
+        
+        // if(s=='a' || s=='e' ||s=='i'||s=='o'|| s=='u'){
+        //     System.out.println("Character is vowel");
+
+        // }
+        // else{
+        //     System.out.println("character is constant");
+        // }
+
+
+
+//Greatest among three numbers
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        int c=sc.nextInt();
+
+        if(a>b && a>c){
+            System.out.println("A is greater");
+        }
+        else if(b>a && b>c){
+            System.out.println("B is greater");
+        }
+        else{
+            System.out.println("C is greater");
+
+        }
+
+
+//sum of natural numbers
+        // int n = sc.nextInt();
+        // int sum=0;
+        // for(int i=0; i<=n;i++){
+        //     sum= sum+i;
+        // }
+        // System.out.println("the Sum of natural nummber is"+sum);
+
+        //Leap year
+        // int year=sc.nextInt();
+        // if((year%400==0 )|| (year%100!=0) &&  (year%4==0)){
+        //     System.out.println("Leap Year ");
+        // }
+        // else{
+        //     System.out.println("Not a leap year");
+        // }
+
+//Simple Calculator
+        // System.out.println("enter the first number");
+        // int a = sc.nextInt();
+        // System.out.println("enter the second5 number");
+
+        
+
+        // int b= sc.nextInt();
+
+        // System.out.println("Enter 1 for addition");
+        // System.out.println("Enter 2 for subtraction");
+        // System.out.println("Enter 3 for multiplication");
+        // System.out.println("Enter 4 for divison");
+
+        // int choice=sc.nextInt();
+        // switch(choice){
+        //     case 1:
+        //       int addition=a+b;
+        //       System.out.println("the addition is "+addition);
+        //       break;
+        //     case 2:
+        //       int subtraction= a-b;
+        //       System.out.println("the subtraction is "+subtraction);
+        //       break;
+
+        //     case 3:
+        //        int multiplication=a*b;
+        //        System.out.println("the multiplication is "+multiplication);
+        //        break;
+            
+        //     case 4:
+        //        int division= a/b;
+        //        System.out.println("the division is "+division); 
+        //        break;
+        //     default:
+        //        System.out.println("enter the correct choice");     
+
+
+
+
+        // }
+        
+// factorial of a number
+    //  int num = sc.nextInt();
+    //  int fact=1;
+    //  for(int i=1; i<=num ; i++){
+    //     fact=fact*i;
+
+    //  }
+    //  System.out.println("the factorial is"+fact);
+    
+
+//generate multiplication
+    // int num= sc.nextInt();
+    // int sum=0;
+    // for(int i =1; i<=10;i++){
+    //     sum=num*i;
+    //     System.out.println(sum);
+    // }
+    
+
+// //fibonacci series
+    // System.out.println("enter the number of fibonacci series you want to generate");
+    // int num= sc.nextInt();
+    // int n1 =0;
+    // int n2 =1;
+    // int n3;
+    // System.out.print(n1);
+    // System.out.print(n2);
+
+    // for (int i=0;i<num;i++){
+    //     n3=n1+n2;
+    //     n1=n2;
+    //     n2=n3;
+    //     System.out.print(n3);
+    // }
+//swap between two numbers
+    // int a=10;
+    // int b=20;
+    // int temp;
+
+
+    // temp=a;
+    // a=b;
+    // b=temp;
+
+    // System.out.println(a);
+    // System.out.println(b);
+    }
+
+}
+    
+
